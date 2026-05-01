42d221923b9b4c9d619e4214503257937b386dd2
Romain Jourdan <23378066+rjourdan@users.noreply.github.com>
2026-05-01 15:12:54 +0000

    xCode project created + fixes

---
+++ _Podedge_sources/Podedge/Podedge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Podedge
+//
+//  Created by Jourdan, Romain on 01.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ _Podedge_sources/Podedge/Podedge/PodedgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodedgeApp.swift
+//  Podedge
+//
+//  Created by Jourdan, Romain on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PodedgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
